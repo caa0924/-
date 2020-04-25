c6080413652a9f1d8869b9327eca61658110122f
czz <651595895@qq.com>
2020-04-25 11:23:44 +0800

    tet02

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ///这是本地分支的练习
+        //这是远程分值的练习
     }
 }
